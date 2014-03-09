a5360a0f95998e55fc2d32ef99580a5e50bfb924
zyy <692895299@qq.com>
2014-03-09 22:07:45 +0800

    ArrayCopy代码提交

---
+++ mycode-j2se/src/main/java/com/yycoder/mycodej2se/ArrayCopy.java
@@ -0,0 +1,19 @@
+package com.yycoder.mycodej2se;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: yycoder
+ * Date: 14-3-9
+ * Time: 下午9:55
+ * To change this template use File | Settings | File Templates.
+ */
+public class ArrayCopy {
+    public static void main(String[] args) {
+        int[] a = new int[]{1, 2, 3, 4};
+        int[] b = new int[4];
+        System.arraycopy(a, 0, b, 0, a.length);
+        for (int j : b) {
+            System.out.print(j + " ");
+        }
+    }
+}
