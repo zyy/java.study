9daf358798c14fd3e0237e01a85f24ff115ba460
赵洋洋 <yangyang.zhao@mtime.com>
2017-08-02 18:40:39 +0800

    spring cloud

---
+++ study-spring-cloud/src/main/java/com/yycoder/spring/cloud/Application.java
@@ -0,0 +1,16 @@
+package com.yycoder.spring.cloud;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by yangyang.zhao on 2017/8/2.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+}
