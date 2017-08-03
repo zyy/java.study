387bf166c660d4b5042469a239b69fcc56e52ec8
赵洋洋 <yangyang.zhao@mtime.com>
2017-08-03 15:21:03 +0800

    spring boot

---
+++ study-spring-boot/src/main/java/com/yycoder/spring/boot/Example.java
@@ -0,0 +1,23 @@
+package com.yycoder.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by yangyang.zhao on 2017/8/3.
+ */
+@RestController
+@EnableAutoConfiguration
+public class Example {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Example.class, args);
+    }
+}
