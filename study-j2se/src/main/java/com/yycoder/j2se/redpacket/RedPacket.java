b509282af6b733d13be1c025ee487f0ec168c323
赵洋洋 <yangyangzhao10@creditease.cn>
2020-09-13 11:34:48 +0800

    红包算法

---
+++ study-j2se/src/main/java/com/yycoder/j2se/redpacket/RedPacket.java
@@ -0,0 +1,11 @@
+package com.yycoder.j2se.redpacket;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description: 红包算法
+ *
+ * @author: yangyangzhao
+ * @date: 2020-09-13 上午11:30
+ */
+public class RedPacket {
+}
